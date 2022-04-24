1af87178ea2b9359ab94843314394ae40cadc241
memetrollsXD <paris@memetrolls.net>
2022-04-25 01:45:54 +0200

    fix event handler

--- src/events/eventHandler.ts
+++ src/events/eventHandler.ts
@@ -1,22 +1,25 @@
 import fs from 'fs';
 
-type Events = { root: [{ [key: string]: Function }] };
-type Event = { [key: string]: Function };
+type Events = {
+    root: {
+        name: string,
+        call: (...args: any[]) => void;
+    }[];
+};
 
 export default async function getEvents(): Promise<Events> {
     const files = await fs.readdirSync('./src/events');
     const events: Events = {
-        root: [{
-            _unusedevent: () => { }
-        }]
+        root: []
     };
 
-    files.forEach((file: any) => {
+    await files.forEach((file: any) => {
         if (file.endsWith('.ts') && !file.endsWith('eventHandler.ts')) {
             import(`./${file}`).then(async (event) => {
                 events.root.push({
-                    [file.replace('.ts', '')]: event.default
-                });
+                    name: file.replace('.ts', ''),
+                    call: event.default
+                })
             });
         }
     });
@@ -25,6 +28,8 @@ export default async function getEvents(): Promise<Events> {
 }
 
 export function findEvent(events: Events, target: string): Function | undefined {
-    const event: Event | undefined = events.root.find((event: Event) => Object.keys(event)[0] === target);
-    if (event) return event[target];
+    const event = events.root.find((e) => e.name === target);
+    if (event) return event.call;
+    console.warn(`Event ${target} not found.`);
+    return undefined;
 }
