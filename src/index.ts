df86e725d296475f678e3e110d2ffe2bdb074ea1
memetrollsXD <memetrollsxd@gmail.com>
2022-04-24 22:59:45 +0200

    Half assed solution while I look more into the issue

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import { REST } from '@discordjs/rest';
-import { Client, Intents } from 'discord.js';
+import { Client, Guild, Intents } from 'discord.js';
 import getConfig from './util/config';
 import register from './util/registercommands';
 import getEvents, { findEvent } from './events/eventHandler';
@@ -31,12 +31,61 @@ client.on('interactionCreate', async (interaction) => {
     });
 });
 
+import source from './db/source.json';
+
+function buildSearch(substrings: string[]) {
+    return new RegExp(
+        substrings
+            .map(function (s) { return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); })
+            .join('{1,}|') + '{1,}'
+        , 'gi');
+}
+
+const regexList: RegExp[] = [];
+const actionList: { action: string; keywords: string[]; }[] = [];
+source.forEach(s => {
+    regexList.push(buildSearch(s.keywords));
+    actionList.push({ action: s.action, keywords: s.keywords });
+});
+
 client.on('messageCreate', async (message) => {
-    await registerEvent('messageCreate', message);
+    // await registerEvent('messageCreate', message);
+    if (message.author.bot) return;
+    if (message.channel.id != '965284036333424722') return;
+    regexList.some(regex => {
+        if (regex.test(message.content)) {
+            const action = actionList.find(a => a.keywords.some(k => regex.test(k)));
+            message.react('👀');
+            action ? message.channel.send(action.action) : '';
+        }
+    });
 });
 
 client.on('messageReactionAdd', async (reaction, user) => {
-    await registerEvent('messageReactionAdd', reaction, user);
+    // await registerEvent('messageReactionAdd', reaction, user);
+    const guild: Guild | undefined = client.guilds.cache.get(`965284035985305680`);
+    if (reaction.partial) {
+        try {
+            await reaction.fetch();
+        } catch (error) {
+            console.error('Fetching message failed: ', error);
+            return;
+        }
+    }
+    if (!user.bot) {
+        const member = guild?.members.cache.find(member => member.id === user.id);
+        if (!member || !reaction.count) return;
+
+        if (reaction.emoji.name == '❌') { // Contributors can delete messages by reacting with a red cross
+            if (member.roles.highest.id == `965284035985305682` && reaction.count >= 2) {
+                reaction.message.delete();
+            }
+        }
+
+        if (reaction.emoji.name == 'pato' && reaction.message.id == `967820613362282526`) { // Pato
+            member.roles.remove(`967814912250380338`);
+        }
+    }
 });
 
 client.on('guildMemberAdd', async (member) => {
