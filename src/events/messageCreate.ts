e70753ce254b6fe0fd19d2265bd70f8c1d4b58a8
memetrollsXD <memetrollsxd@gmail.com>
2022-04-25 16:32:33 +0200

    Just do the whole thing

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,7 +1,7 @@
 import source from '../db/source.json';
 import Logger from '../util/Logger';
-import {Message} from "discord.js";
-import Tesseract from "tesseract.js";
+import { Message } from "discord.js";
+import detect from '../util/ocr';
 const c = new Logger('messageCreate');
 
 function buildSearch(substrings: string[]) {
@@ -18,31 +18,21 @@ const regexList: RegExp[] = [];
 const actionList: { action: string; keywords: string[]; }[] = [];
 source.forEach(s => {
     regexList.push(buildSearch(s.keywords));
-    actionList.push({action: s.action, keywords: s.keywords});
+    actionList.push({ action: s.action, keywords: s.keywords });
 });
 
 export default async function run(message: Message) {
     if (message.author.bot) return;
     c.trail(`<${message.author.username}#${message.author.discriminator}> ${message.content}`);
     if (message.channel.id != '965284036333424722') return;
-    let imageOcr = "";
-    if (message.attachments.size > 0) {
-        let url = message.attachments.first()?.url;
-        if (url != null && (url.endsWith('.png') || url.endsWith('.jpg'))) {
-            const recognizeResult = await Tesseract.recognize(url);
-            imageOcr = recognizeResult.data.text;
-        }
-    }
-    if (message.content.endsWith('.png') || message.content.endsWith('.jpg')) {
-        const recognizeResult = await Tesseract.recognize(message.content);
-        imageOcr = recognizeResult.data.text;
-    }
+    const imageOcr = await detect(message);
+
     regexList.some(regex => {
         if (regex.test(message.content) || regex.test(imageOcr)) {
             const action = actionList.find(a => a.keywords.some(k => regex.test(k)));
             message.react('👀');
             if (action) {
-                message.channel.send(action.action);
+                message.reply(action.action);
                 c.trail(`Match found for ${action.keywords[0]}`)
             }
         }
