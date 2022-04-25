d33ccc275f222c009fb03fb9bbe8a337dfded101
muhammadeko <muhammadekoprasetyo29@gmail.com>
2022-04-25 20:37:10 +0700

    give the bot ocr powaaah

--- src/events/messageCreate.ts
+++ src/events/messageCreate.ts
@@ -1,11 +1,16 @@
 import source from '../db/source.json';
 import Logger from '../util/Logger';
+import {Message} from "discord.js";
+
+const tesseract = require('node-tesseract-ocr');
 const c = new Logger('messageCreate');
 
 function buildSearch(substrings: string[]) {
     return new RegExp(
         substrings
-            .map(function (s) { return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&'); })
+            .map(function (s) {
+                return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
+            })
             .join('{1,}|') + '{1,}'
         , 'gi');
 }
@@ -14,15 +19,25 @@ const regexList: RegExp[] = [];
 const actionList: { action: string; keywords: string[]; }[] = [];
 source.forEach(s => {
     regexList.push(buildSearch(s.keywords));
-    actionList.push({ action: s.action, keywords: s.keywords });
+    actionList.push({action: s.action, keywords: s.keywords});
 });
 
-export default async function run(message: any) {
+export default async function run(message: Message) {
     if (message.author.bot) return;
     c.trail(`<${message.author.username}#${message.author.discriminator}> ${message.content}`);
     if (message.channel.id != '965284036333424722') return;
+    let imageOcr = "";
+    if (message.attachments.size > 0) {
+        let url = message.attachments.first()?.url;
+        if (url != null && (url.endsWith('.png') || url.endsWith('.jpg'))) {
+            imageOcr = await tesseract.recognize(url);
+        }
+    }
+    if (message.content.endsWith('.png') || message.content.endsWith('.jpg')) {
+        imageOcr = await tesseract.recognize(message.content);
+    }
     regexList.some(regex => {
-        if (regex.test(message.content)) {
+        if (regex.test(message.content) || regex.test(imageOcr)) {
             const action = actionList.find(a => a.keywords.some(k => regex.test(k)));
             message.react('👀');
             if (action) {
