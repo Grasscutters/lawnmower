9a446e165ceb8e53fbad7dfb4d5a89f10633b9a8
memetrollsXD <memetrollsxd@gmail.com>
2022-04-25 16:36:32 +0200

    Some logging

--- src/util/ocr.ts
+++ src/util/ocr.ts
@@ -1,6 +1,8 @@
 import { Message } from "discord.js";
 import Tesseract from "tesseract.js";
 import fetch from 'node-fetch';
+import Logger from "./Logger";
+const c = new Logger("OCR", "blue");
 
 async function getBufferFromURL(url: string): Promise<Buffer | string> {
     const img = await fetch(url);
@@ -30,7 +32,7 @@ export default async function detect(message: Message): Promise<string> {
             imageOcr = recognizeResult.data.text;
         } catch { }
 
-        console.log(imageOcr);
+        c.log(`Text detected on ${message.id} (by ${message.member?.nickname}): ${imageOcr}`);
         return imageOcr;
     } catch (error) {
         console.log(error);
