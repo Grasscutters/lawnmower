65536bd93bf7f72baaea35a1e032226041da2d6a
memetrollsXD <memetrollsxd@gmail.com>
2022-04-25 16:32:33 +0200

    Just do the whole thing

---
+++ src/util/ocr.ts
@@ -0,0 +1,39 @@
+import { Message } from "discord.js";
+import Tesseract from "tesseract.js";
+import fetch from 'node-fetch';
+
+async function getBufferFromURL(url: string): Promise<Buffer | string> {
+    const img = await fetch(url);
+    try {
+        return Buffer.from(await img.arrayBuffer());
+    } catch (e) {
+        return '';
+    }
+}
+
+export default async function detect(message: Message): Promise<string> {
+    try {
+        let imageOcr: string = "";
+        const buffer: Buffer = message.attachments.first()?.attachment as Buffer;
+
+        // Handle attachments
+        if (message.attachments.size > 0) {
+            if (buffer != null) {
+                const recognizeResult = await Tesseract.recognize(buffer);
+                imageOcr = recognizeResult.data.text;
+            }
+        }
+
+        // Handle URLs
+        try {
+            const recognizeResult = await Tesseract.recognize(await getBufferFromURL(message.content));
+            imageOcr = recognizeResult.data.text;
+        } catch { }
+
+        console.log(imageOcr);
+        return imageOcr;
+    } catch (error) {
+        console.log(error);
+        return "";
+    }
+}
