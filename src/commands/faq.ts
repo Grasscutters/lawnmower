c8a6bbfe1715ffa43b0186a814a12a87cd06edf9
memetrollsXD <memetrollsxd@gmail.com>
2022-05-15 17:22:57 +0200

    FAQ Command

---
+++ src/commands/faq.ts
@@ -0,0 +1,40 @@
+import { SlashCommandBuilder } from '@discordjs/builders';
+import { CommandInteraction, MessageEmbed, TextChannel } from 'discord.js';
+
+async function run(interaction: CommandInteraction) {
+    const embed = new MessageEmbed();
+    const input = {
+        question: interaction.options.getString('question') || "",
+        answer: interaction.options.getString('answer') || "",
+    }
+    embed.setTitle(input.question)
+        .setDescription(input.answer)
+        .setColor('#9f2b2b')
+        .setFooter({
+            text: `${interaction.member!.user.username} • ${new Date().toLocaleString()}`,
+            iconURL: `https://cdn.discordapp.com/avatars/${interaction.member!.user.id}/${interaction.member!.user.avatar}.png`
+        });
+
+    interaction.reply({
+        ephemeral: true,
+        embeds: [embed],
+        content: '** **',
+    });
+
+    (interaction.client.channels.cache.get('974428071811756052') as TextChannel).send({
+        embeds: [embed],
+        content: '** **'
+    });
+}
+
+const cmd = new SlashCommandBuilder()
+    .setName('faq')
+    .setDescription('Post to #faq channel')
+    .addStringOption(o => o.setName('question').setRequired(true).setDescription('question'))
+    .addStringOption(o => o.setName('answer').setRequired(true).setDescription('answer'))
+
+let _;
+export default _ = {
+    process: run,
+    command: cmd
+}
