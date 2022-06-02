7112e2e281ed094d35b734b3726ec17ec927c119
memetrollsXD <memetrollsxd@gmail.com>
2022-06-02 13:00:42 +0200

    Update messageUpdate.ts

--- src/events/messageUpdate.ts
+++ src/events/messageUpdate.ts
@@ -4,7 +4,7 @@ import sendToLog from "../util/sendToLog";
 const c = new Logger("messageUpdate", "cyan");
 
 export default async function run(oldMessage: Message<boolean> | PartialMessage, newMessage: Message<boolean> | PartialMessage) {
-    if (newMessage.author!.bot) return;
+    if (newMessage.author?.bot) return;
 
     c.log(`Message edited by ${newMessage.author?.tag || ""} (${newMessage.id})`);
     if (newMessage.cleanContent) c.trail(newMessage.cleanContent);
