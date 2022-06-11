2ec79a613e876a4dac09450809f83f5ef90a5b69
memetrollsXD <memetrollsxd@gmail.com>
2022-06-11 23:27:06 +0200

    proper safe reaction message

--- src/events/messageReactionAdd.ts
+++ src/events/messageReactionAdd.ts
@@ -2,6 +2,9 @@ import { Client, Guild, Message, MessageReaction, PartialMessageReaction, Partia
 import Logger from "../util/Logger";
 const c = new Logger("messageReactionAdd");
 
+// #general, #general-zh, #general-other, #development, #development-zh, #plugin-dev
+const WHITELISTED_CHANNELS = [965284035985305689, 973699269557112882, 966835424603537448, 965619953900326973, 970935992032825364, 970830900227100742]
+
 export default async function run(reaction: MessageReaction | PartialMessageReaction, user: User | PartialUser, client: Client) {
     const guild: Guild | undefined = client.guilds.cache.get(`965284035985305680`);
     if (reaction.partial) {
@@ -12,15 +15,15 @@ export default async function run(reaction: MessageReaction | PartialMessageReac
             return;
         }
     }
-    if (!user.bot) {
-        const member = guild?.members.cache.find(member => member.id === user.id);
-        if (!member || !reaction.count) return;
+
+    if (!user.bot && !reaction.message.author?.bot) {
+        if (!reaction.count) return;
 
         if (reaction.emoji.name == '🔨' && reaction.count < 2) {
-            reaction.message.reply(`Please read <#978197435056787597>.`);
-            setInterval(() => {
-                reaction.message.delete().catch();
-            }, 5000);
+            const msg = await reaction.message.reply(`Please read <#978197435056787597>.`);
+            setTimeout(() => {
+                msg.delete();
+            }, 3000);
         }
     }
 }
