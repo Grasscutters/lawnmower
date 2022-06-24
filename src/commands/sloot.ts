be108ee541ca9d8ce196fb756f1c0c20e3d9baa8
Kimi <34180607+Kimi898246@users.noreply.github.com>
2022-06-25 02:01:56 +0800

    Repurposing the command
    Since the first version wasn't too good, i switched the picture to better one.

--- src/commands/sloot.ts
+++ src/commands/sloot.ts
@@ -2,7 +2,7 @@ import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 
 async function run(interaction: CommandInteraction) {
-    interaction.reply('https://crepe.moe/Sd3NhaT3');
+    interaction.reply('https://crepe.moe/jaCki9CJ');
 }
 
 const cmd = new SlashCommandBuilder()
