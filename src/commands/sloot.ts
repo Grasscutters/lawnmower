790ee25227a8440fea855e1796478d2045e21268
KingRainbow44 <kobedo11@gmail.com>
2022-06-25 14:33:14 -0400

    random sloot (needs extensive testing)

--- src/commands/sloot.ts
+++ src/commands/sloot.ts
@@ -1,13 +1,14 @@
 import { SlashCommandBuilder } from '@discordjs/builders';
 import { CommandInteraction } from 'discord.js';
 import fs from 'fs'
-import sloot from '../db/sloot.json';
+
+const sloot: {[key: string]: string} = require('../db/sloot.json');
 
 async function run(interaction: CommandInteraction) {
     let list = Object.keys(sloot).join(", ")
     switch (interaction.options.getSubcommand(true)) {
         case 'list':
-            interaction.reply({
+            return await interaction.reply({
                 embeds: [{
                     title: `Sloot List`,
                     description: `${list}`,
@@ -18,28 +19,33 @@ async function run(interaction: CommandInteraction) {
                 }],
                 ephemeral: true
             })
-            break;
         case 'fetch':
-            let name = interaction.options.getString('name', true)
-            if (!sloot[name])
-                return interaction.reply({ content: 'No key found', ephemeral: true })
-            else
-                interaction.reply({ content: sloot[name] })
+            let name: string|null = interaction.options.getString('name', false);
+            if(!name) {
+                if(Object.values(sloot).length < 1)
+                    return interaction.reply({content: 'No content found.', ephemeral: true});
+                
+                let random: string = Object.values(sloot)[Math.floor(Math.random() * Object.keys(sloot).length)];
+                return interaction.reply({content: random});
+            } else {
+                if(!sloot[name])
+                    return interaction.reply({content: 'No key found.', ephemeral: true});
+                else return interaction.reply({content: sloot[name]});
+            }
             break;
         case 'add':
             let key = interaction.options.getString('key', true)
             let value = interaction.options.getString('value', true)
-            let json = JSON.parse(sloot)
-            json[key] = value
-            let data = JSON.stringify(json, null, 2)
-            fs.writeFileSync('../db/sloot.json', data)
-            interaction.reply({content: 'Added', ephemeral: true})
-            break;
+            sloot[key] = value
+            
+            let data = JSON.stringify(sloot, null, 2)
+            fs.writeFileSync(`${process.cwd()}/src/db/sloot.json`, data)
+            return await interaction.reply({content: 'Added', ephemeral: true});
         default:
-            interaction.reply({
+            return await interaction.reply({
                 content: 'Unknown subcommand',
                 ephemeral: true
-            })
+            });
     }
 }
 
@@ -48,7 +54,7 @@ const cmd = new SlashCommandBuilder()
     .setDescription('Sloot behaviour')
     .addSubcommand(o => o.setName('fetch')
         .setDescription('Fetches an image from the db')
-        .addStringOption(o => o.setName('name').setDescription('key for the link to fetch from db').setRequired(true))
+        .addStringOption(o => o.setName('name').setDescription('key for the link to fetch from db'))
     )
     .addSubcommand(o => o.setName('add')
         .setDescription('Adds an image from the db')
