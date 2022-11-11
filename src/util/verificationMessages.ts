01eae420393c189209724d71f9684fa267175e63
Scald <104459145+Arikatsu@users.noreply.github.com>
2022-11-12 04:41:18 +0530

    Add `verify` button

--- src/util/verificationMessages.ts
+++ src/util/verificationMessages.ts
@@ -1,10 +1,18 @@
-import { CommandInteraction, MessageEmbed, TextChannel } from 'discord.js';
+import { CommandInteraction, MessageEmbed, TextChannel, MessageActionRow, MessageButton } from 'discord.js';
 import vm from '../db/verificationMessages.json';
 import Logger from './Logger';
 const c = new Logger(`verificationMessages`);
 
+const verificationRow = new MessageActionRow()
+    .addComponents(
+        new MessageButton()
+            .setCustomId('verification-button')
+            .setLabel('Verify')
+            .setStyle('SUCCESS')
+    );
+
 export default async function run(interaction: CommandInteraction, specify?: string) {
-    vm.forEach(v => {
+    vm.forEach((v, idx, arr) => {
         if (specify && v.lang_code != specify) return;
 
         const embed = new MessageEmbed();
@@ -14,9 +22,11 @@ export default async function run(interaction: CommandInteraction, specify?: str
             .setFooter({
                 text: new Date().toLocaleString()
             });
+
         (interaction.client.channels.cache.get('1028327705571238009') as TextChannel).send({
             embeds: [embed],
-            content: '** **'
+            content: '** **',
+            components: (idx == arr.length - 1) ? [verificationRow] : []
         });
     });
 }
